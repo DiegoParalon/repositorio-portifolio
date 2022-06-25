ff87c2c7360b8cc172d6e9df8d92cde15f37a9f1
Diego Paralon <diegoparalon@hotmail.com>
2022-06-25 14:18:16 +0100

    Fix responsive bugs

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const menuBtn = document.querySelector('.navbar')
 const scrollBtn = document.querySelector('.scroll-up-btn')
+const menuOpenBtn = document.querySelector('.menu-btn-open')
+const menuCloseBtn = document.querySelector('.menu-btn-x')
 let abertoFechado = false
 
 function openClose() {
     if (abertoFechado === false) {
         menuBtn.classList.add('active')
+        menuOpenBtn.classList.add('open')
+        menuCloseBtn.classList.add('hidden')
         abertoFechado = true
     } else {
         menuBtn.classList.remove('active')
+        menuCloseBtn.classList.remove('hidden')
+        menuOpenBtn.classList.remove('open')
         abertoFechado = false
     }
     
